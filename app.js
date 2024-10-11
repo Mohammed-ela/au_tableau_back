2355222881197d814b5228cd8af8b35b163c42ee
Mohammed <elamrani.mohammed95@gmail.com>
2024-10-11 16:50:46 +0200

    update bdd & env example

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
 const express = require("express");
-const port = process.env.PORT || 5000;
 const helmet = require("helmet");
 const cors = require("cors");
-
+const classroomRoutes = require("./routes/classroomRoutes");
+const studentRoutes = require("./routes/studentRoutes");
 const app = express();
-
-// Sécurisation des en-têtes 
+const port = process.env.PORT || 5000;
+//bdd 
+require("./config/database"); 
+// Sécurisation des en-têtes
 app.use(helmet());
-//activer les cors pour accepter all domaine
+// Activer les CORS
 app.use(cors());
-
-// Route pour les webhooks Stripe (appel au debut pour eviter les bug)
-const webhook = require("./routes/webhook");
-app.use('/webhook', webhook);
-
-// requete en json (alternatiev to bodyparser)
+// Gestion des requêtes JSON
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({ message: "Bienvenue sur mon API en NodeJS" });
+// Routes API
+app.use("/classrooms", classroomRoutes);
+app.use("/classrooms/:classroom_id/students", studentRoutes);
+
+// Page d'accueil
+app.get("/", (req, res) => {
+    res.json({ message: "Bienvenue sur mon API de gestion de soutenances" });
 });
 
+// Lancer le serveur
 app.listen(port, () => {
     console.log(`Serveur en ligne sur le port : ${port}`);
 });
