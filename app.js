632f2d191d42556f0594b60097b18f93ed9f7a71
Mohammed <elamrani.mohammed95@gmail.com>
2024-10-11 15:13:39 +0200

    git init

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const port = process.env.PORT || 5000;
+const helmet = require("helmet");
+const cors = require("cors");
+
+const app = express();
+
+// Sécurisation des en-têtes 
+app.use(helmet());
+//activer les cors pour accepter all domaine
+app.use(cors());
+
+// Route pour les webhooks Stripe (appel au debut pour eviter les bug)
+const webhook = require("./routes/webhook");
+app.use('/webhook', webhook);
+
+// requete en json (alternatiev to bodyparser)
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({ message: "Bienvenue sur mon API en NodeJS" });
+});
+
+app.listen(port, () => {
+    console.log(`Serveur en ligne sur le port : ${port}`);
+});
