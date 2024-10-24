04e6b132e242c084483ad252e0bf8084417738b3
Mohammed <elamrani.mohammed95@gmail.com>
2024-10-24 22:19:35 +0200

    update bdd

--- bdd.js
+++ bdd.js
@@ -2,22 +2,20 @@ const { MongoClient } = require('mongodb');
 
 const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}.mongodb.net/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`;
 
-let db;
+let db = null;
 
 const connectDB = async () => {
-    if (db) {
-        return db;
+    if (!db) {
+        try {
+            const client = await MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+            console.log("MongoDB connecté");
+            db = client.db(process.env.MONGODB_DATABASE);
+        } catch (err) {
+            console.error("Erreur lors de la connexion à MongoDB:", err);
+            process.exit(1); 
+        }
     }
-    try {
-        const client = new MongoClient(uri);
-        await client.connect();
-        console.log("mongo success");
-        db = client.db(process.env.MONGODB_DATABASE); 
-        return db;
-    } catch (err) {
-        console.error("erreur bdd =>", err);
-        process.exit(1); 
-    }
+    return db;
 };
 
 module.exports = connectDB;
